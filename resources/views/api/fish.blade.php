a53b49260cfbd87fc9e9feca79db9d69079466fa
jmuribe10 <juanmartin104@gmail.com>
2022-05-24 22:40:15 -0500

    api oute

--- resources/views/api/fish.blade.php
+++ resources/views/api/fish.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.app')
-
 @section('content')
 <section>
     <div class="row justify-content-center">
--- resources/views/game/index.blade.php
+++ resources/views/game/index.blade.php
@@ -5,6 +5,7 @@
 
 <div class="row">
   @foreach ($viewData["games"] as $game)
+  <a href="{{ route('fish.index')}}"> @lang("Hungry")</a>
   <div class="col-md-4 col-lg-3 mb-2">
     <div class="card">
     <img class="image rounded-center" src="{{asset('/uploads/games/'.$game->getImage() )}}" alt="image" style="align:center width: 320px;height: 320px; padding: 10px; margin: 0px; ">
